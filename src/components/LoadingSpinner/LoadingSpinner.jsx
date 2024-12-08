6fa6232eaf324c92095154bba07fb748ef22fd87
Kwame Baffoe <kwamebaffoe@Kwames-MacBook-Pro.local>
2024-12-08 00:19:13 -0800

    we added more design to the prelaoder

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,31 +1,50 @@
-// LoadingSpinner.jsx
 import React from 'react';
 import './LoadingSpinner.css';
 
-export const LoadingSpinner = ({ size = 'default', color = 'primary', fullScreen = false }) => {
-  const sizeClasses = {
-    small: 'spinner-small',
-    default: 'spinner-default',
-    large: 'spinner-large'
-  };
-
-  const colorClasses = {
-    primary: 'spinner-primary',
-    white: 'spinner-white',
-    gray: 'spinner-gray',
-    success: 'spinner-success',
-    warning: 'spinner-warning',
-    danger: 'spinner-danger'
-  };
-
+export const LoadingSpinner = ({ 
+  size = 'default', 
+  color = 'primary', 
+  fullScreen = false,
+  text = 'Loading...'
+}) => {
   return (
     <div className={`loading-spinner-wrapper ${fullScreen ? 'full-screen' : ''}`}>
-      <div className={`spinner ${sizeClasses[size]} ${colorClasses[color]}`}>
-        <div className="spinner-inner"></div>
+      {fullScreen && (
+        <>
+          <div className="loading-background">
+            <div className="background-gradient"></div>
+            <div className="background-particles">
+              <div className="particle"></div>
+              <div className="particle"></div>
+              <div className="particle"></div>
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="spinner-container">
+        <div className={`spinner ${size ? `spinner-${size}` : ''} ${color ? `spinner-${color}` : ''}`}>
+          <div className="glow"></div>
+          <div className="spinner-ring ring-outer"></div>
+          <div className="spinner-ring ring-middle"></div>
+          <div className="spinner-ring ring-inner"></div>
+          
+          <div className="logo-container">
+            <span className="logo-text">P</span>
+          </div>
+        </div>
       </div>
+
+      {fullScreen && (
+        <>
+          <div className="loading-text">{text}</div>
+          <div className="loading-progress">
+            <div className="progress-bar"></div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
-// Add a default export as well for flexibility
 export default LoadingSpinner;
